463d0a8a2f7ab2671bc6d88512fd50297b92b62b
Fad <feescorciam@protonmail.com>
2021-05-05 11:13:25 -0500

    bootrtap from cdn

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, make_response, redirect, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask, request, make_response, redirect, render_template, session
 
 app = Flask(__name__)
-Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 @app.errorhandler(404)
 def not_found(error):
@@ -17,14 +17,14 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect("/hello"))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 @app.route('/hello')
 def hello():
     todos = ['TODO 1','TODO 2','TODO 3']
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         "user_ip":user_ip,
         "todos":todos
